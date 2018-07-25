92f1ab8e8524461f7c1d9f89e2072c71baa1bb0f
KasperTatia <kaspariants.94@gmail.com>
2018-07-25 20:16:42 +0300

    Init commit

---
+++ src/com/sourceit/Lesson4.java
@@ -0,0 +1,17 @@
+package com.sourceit;
+
+public class Lesson4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+//		MKyong.com  изучить
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Kasper
+ *
+ */
+module MyProject {
+}
