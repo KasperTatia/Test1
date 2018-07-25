c10abed7ffd73368f3f6d04249a506d1fdc986ac
KasperTatia <kaspariants.94@gmail.com>
2018-07-25 20:57:35 +0300

    hw change

--- src/com/sourceit/Lesson5.java
+++ src/com/sourceit/Lesson5.java
@@ -2,9 +2,9 @@ package com.sourceit;
 
 public class Lesson5 {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		// TODO Auto-generated method stub
-
+ System.out.println("HW1");
 	}
 
 }
